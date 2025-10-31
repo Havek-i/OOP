c0ba14521f917f43793b0563f3557da50e16b279
Havek-i <ivavasilev2004@gmail.com>
2025-10-31 16:32:01 +0700

    Добавление кода для ПР24

---
+++ PR24/decorators.py
@@ -0,0 +1,65 @@
+from abc import ABC, abstractmethod
+import time
+
+class BaseDecorate(ABC):
+    '''Абстрактный клас для будющих декораторов'''
+
+    @abstractmethod
+    def __init__(self, func):
+        self.func = func
+
+    @abstractmethod
+    def __call__(self, *args, **kwds):
+        '''Абстрактный метод, который вызывается при вызове функции'''
+        pass
+
+class LoggerDecorate(BaseDecorate):
+    '''Декоратор логирования функций'''
+
+    def __init__(self, func):
+        super().__init__(func)
+    
+    def __call__(self, *args, **kwds):
+        with open('./PR24/log.txt', 'a+', encoding='utf-8') as f:
+            f.write(f'Function {self.func} was started\n')
+            result = self.func(*args, **kwds)
+            f.write(f'Function {self.func} was finished\n')
+            print('Отчёт представлен в log.txt')
+            return result
+
+
+class CountDecorate(BaseDecorate):
+    '''Декоратор подсчёта количества вызова функции'''
+
+    def __init__(self, func):
+        super().__init__(func)
+        self.count = 0
+
+    def __call__(self, *args, **kwds):
+        self.count += 1
+        print(f'Функция {self.func} вызвана {self.count} {'раза' if str(self.count)[-1] in ('2', '3', '4') else 'раз'}')
+        return self.func(*args, **kwds)
+    
+
+def time_decorator(func):
+    '''Функция-декоратор, которая заменяет класс-декоратор decorators.TimeDecorate'''
+    def wrapper(*args, **kwgs):
+        start = time.time()
+        result = func(*args, **kwgs)
+        end = time.time()
+        print(f'Функция {func} отработала за {(end - start):.8f} сек.')
+        return result
+    return wrapper
+
+# class TimeDecorate(BaseDecorate):
+#     '''Декоратор подсчёта времени работы функции'''
+
+#     def __init__(self, func):
+#         super().__init__(func)
+    
+#     def __call__(self, *args, **kwds):
+#         start_time = time.time()
+#         result = self.func(*args, **kwds)
+#         end_time = time.time()
+#         print(f'Функция {self.func} отработала за {(end_time - start_time):.4f} сек.')
+#         return result
---
+++ PR24/main.py
@@ -0,0 +1,33 @@
+from decorators import CountDecorate, LoggerDecorate, time_decorator
+
+@LoggerDecorate
+@CountDecorate
+def summ(a: int, start=0):
+    sum = 0
+    for i in range(start, a + 1):
+        sum += i
+    return sum
+
+@time_decorator
+def minus_sum(a: int, start=0):
+    sum = 0
+    for i in range(start, a + 1):
+        sum -= i
+    return sum
+
+@time_decorator
+def say(text: str):
+    print(text.upper())
+
+# Главная функция
+def main():
+    for i in range(2):
+        print(summ(4, 2))
+
+    print(minus_sum(7, 4))
+    say('Hi!!')
+
+
+# Запуск программы
+if __name__ == '__main__':
+    main()
